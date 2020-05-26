08a0a06b9b63286c490dcb7aa8b1d821b1511bc2
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 22:25:50 -0500

    Program to detect whether a number inserted is positive or negative

---
+++ Utility_of_ELIF.py
@@ -0,0 +1,8 @@
+x=int(input("Write a whole number: "))
+if x>0:
+    print("The number written is positive")
+elif x<0:
+    print("The number written is negative")
+else:
+    print("The number written is ZERO")
+    
